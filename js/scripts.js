6af2e7b7e8d52393744a32c5512aac0fa4ac94d6
hasilproject <67303470+irvansyah-code@users.noreply.github.com>
2020-06-30 11:48:04 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,4 @@
 
-// Klik menu navbar
-
- $('.page-scroll').on('click', function(e) { 
-
- 	// ambil isi href
- 	var tujuan = $(this).attr('href'); 
-
- 	// tangkap element yang bersangkutan
- 	var elemenTujuan = $(tujuan); 
-
- 	// Pindahkan scroll
- 	$('html , body').animate({ 
- 		scrollTop: elemenTujuan.offset().top - 50 
-
- 	}, 1250, 'easeInOutExpo'); 
-
- 	e.preventDefault(); 
-
- });﻿
-
-
-
-
 // panggil menu burger
 const Toggle = document.querySelector ('.toggle input'); 
 const nav = document.querySelector ('nav ul');
